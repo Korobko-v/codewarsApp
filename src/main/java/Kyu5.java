41727903fe7625d2a786d7d182a77fd196553cea
korobko <19kvd95@mail.ru>
2023-04-02 22:29:37 +0300

    help granny

--- src/main/java/Kyu5.java
+++ src/main/java/Kyu5.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Tasks corresponding to the fifth kyu on Codewars
@@ -24,5 +28,23 @@ public class Kyu5 {
         long k = i * j == prod ? 1 : 0;
 
         return new long[]{i, j, k};
+    }
+
+    /**
+     * https://www.codewars.com/kata/5536a85b6ed4ee5a78000035
+     * @param arrFriends array with friends
+     * @param ftwns friends and towns
+     * @param h towns with distances from granny
+     * @return total tour distance
+     */
+    public static int tour(String[] arrFriends, String[][] ftwns, Map<String, Double> h) {
+        List<String[]> filteredTowns = Arrays.stream(ftwns).filter(s -> Arrays.asList(arrFriends).contains(s[0])).collect(Collectors.toList());
+        double tour = (h.get(filteredTowns.get(0)[1]) + h.get(filteredTowns.get(filteredTowns.size()-1)[1]));
+        if (arrFriends.length > 1) {
+            for (int i = 1; i < filteredTowns.size(); i++) {
+                tour += Math.sqrt(Math.pow(h.get(filteredTowns.get(i)[1]), 2) - Math.pow(h.get(filteredTowns.get(i-1)[1]), 2));
+            }
+        }
+        return (int)tour;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Tasks solution methods
@@ -6,6 +8,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        String[] friends1 = new String[] { "A1", "A2", "A3", "A4", "A5" };
+        String[][] fTowns1 = { new String[] {"A1", "X1"}, new String[] {"A2", "X2"}, new String[] {"A3", "X3"},
+                new String[] {"A4", "X4"} };
+        Map<String, Double> distTable1 = new HashMap<String, Double>();
+        distTable1.put("X1", 100.0); distTable1.put("X2", 200.0); distTable1.put("X3", 250.0);
+        distTable1.put("X4", 300.0);
+        System.out.println(Kyu5.tour(friends1, fTowns1, distTable1));
     }
 
 
